9317e3c55efd0622c80e1ef46c3132554e53e404
sivaadda <sivapriyaadda@gmail.com>
2025-06-15 18:33:09 +0530

    trying to fix bug

--- src/dindex.jsx
+++ src/dindex.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
 import "./portalStyles.css";
+import App from "./App";
 // your CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App/>);
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './App.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App/>);
